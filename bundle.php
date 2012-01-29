99ec0f23f18347c45fd12989be12c66eea2bb7a5
Phill Sparks <me@phills.me.uk>
2012-01-29 19:40:39 +0000

    Updated package to be a Laravel 3 bundle.

---
+++ bundle.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Gravatar - A bundle for Laravel based on Damian Bushong's GravatarLib
+ *
+ * Original library by
+ * @author  Damian Bushong
+ * @see  https://github.com/damianb/gravatarlib
+ *
+ * Original Laravel package by
+ * @author  Michael John Owens
+ * 
+ * Updated to Laravel 3 Bundle by:
+ * @author  Phill Sparks <me@phills.me.uk>
+ */
+
+require __DIR__.DS.'gravatar'.EXT;
